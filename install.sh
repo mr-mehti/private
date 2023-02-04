c036d5f519474ed61dbd2c42bc86ba0795c02a54
Mehti <34041379+mr-mehti@users.noreply.github.com>
2023-02-03 18:38:07 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
 set -e
-EXIT_CODE=0
-command || EXIT_CODE=$?
-echo $EXIT_CODE
-sudo apt install unzip -y
+sudo apt install unzip -y || true
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh)
-rm /usr/local/etc/v2ray/ -r
-mkdir /usr/local/etc/v2ray/
-sudo mv -f ~/private/pub.crt /usr/local/etc/v2ray/
-sudo mv -f ~/private/prv.key /usr/local/etc/v2ray/
+mkdir /usr/local/etc/v2ray/ || true
+sudo mv -f ~/private/pub.crt /usr/local/etc/v2ray/ || true
+sudo mv -f ~/private/prv.key /usr/local/etc/v2ray/ || true
 systemctl start v2ray && systemctl enable v2ray
-mv -f ./config.json /usr/local/etc/v2ray
+mv -f ./config.json /usr/local/etc/v2ray || true
 systemctl restart v2ray

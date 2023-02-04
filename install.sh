14e2b9d644b05d05ff6a653b186ad3de61cfb835
Mehti <34041379+mr-mehti@users.noreply.github.com>
2023-02-04 08:32:19 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 set -e
+sudo apt update || true
 sudo apt install unzip -y || true
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh)

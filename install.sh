499f59ee655014940025b1131edfc805b6c25806
Mehti <34041379+mr-mehti@users.noreply.github.com>
2023-02-03 18:06:18 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh)
 rm /usr/local/etc/v2ray/ -r
 mkdir /usr/local/etc/v2ray/
-mv -f ./pub.crt /usr/local/etc/v2ray/
-mv -f ./prv.key /usr/local/etc/v2ray/
+sudo mv -f ~/private/pub.crt /usr/local/etc/v2ray/
+sudo mv -f ~/private/prv.key /usr/local/etc/v2ray/
 systemctl start v2ray && systemctl enable v2ray
 mv -f ./config.json /usr/local/etc/v2ray
 systemctl restart v2ray
